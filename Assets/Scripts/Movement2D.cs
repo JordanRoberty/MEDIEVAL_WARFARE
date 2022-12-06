3fc99767aa20391e59e5f9d458608f4595e107d1
badbois <Jordan.roberty@outlook.fr>
2022-12-06 12:21:43 +0100

    🧼 [Movement2D] Switching from CamelCase to snake_case

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -8,77 +8,81 @@ public class Movement2D : MonoBehaviour
     private Rigidbody2D _rb;
 
     [Header("Layer Masks")]
-    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private LayerMask _ground_layer;
 
     [Header("Movement Variables")]
-    [SerializeField] private float _movementAcceleration = 50f;
-    [SerializeField] private float _maxMoveSpeed = 12f;
-    [SerializeField] private float _groundLinearDrag = 10f; //a.k.a deceleration
-    private float _horizontalDirection;
-    private bool _changingDirection => (_rb.velocity.x > 0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f);
+    [SerializeField] private float _movement_acceleration = 50f;
+    [SerializeField] private float _max_move_speed = 12f;
+    [SerializeField] private float _ground_linear_drag = 10f; //a.k.a deceleration
+    private float _horizontal_direction;
+    private bool _changing_direction => (_rb.velocity.x > 0f && _horizontal_direction < 0f) || (_rb.velocity.x < 0f && _horizontal_direction > 0f);
 
-    [Header("Jump Variables")]
-    [SerializeField] private float _jumpForce = 12f;
-    [SerializeField] private float _airLinearDrag = 2.5f;
-    [SerializeField] private float _fallMultiplier = 8f;
-    [SerializeField] private float _lowJumpFallMultiplier = 5f;
-    [SerializeField] private int _extraJumps = 1;
-    private int _extraJumpsValue;
-    private bool _canJump => Input.GetButtonDown("Jump") && (_onGround || _extraJumpsValue > 0);
+    [Header("jump Variables")]
+    [SerializeField] private float _jump_force = 12f;
+    [SerializeField] private float _air_linear_drag = 2.5f;
+    [SerializeField] private float _fall_gravity = 8f;
+    [SerializeField] private float _low_jump_fall_gravity = 5f;
+    [SerializeField] private int _extra_jumps = 1;
     [SerializeField] private float _fastfall_gravity = 500f; 
 	[SerializeField] private float _fastfall_max_speed = 50.0f;
+    private int _extra_jumps_count;
+    private bool _can_jump => Input.GetButtonDown("Jump") && (_on_ground || _extra_jumps_count > 0); 
 
     [Header("Ground Collision Variable")]
-    [SerializeField] private float _groundRaycastLength;
-    [SerializeField] private Vector3 _groundRaycastOffset;
-    private bool _onGround;
+    [SerializeField] private float _ground_raycast_length;
+    [SerializeField] private Vector3 _ground_raycast_offset;
+    private bool _on_ground;
+
     //crouch variables
     private bool _is_crouching = false;
 
     private void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();    
+        _rb = GetComponent<Rigidbody2D>();  
     }
 
     private void Update()
     {
-        _horizontalDirection = GetInput().x;
-        // I don't know why I had to put the Jump() call here
+        _horizontal_direction = get_input().x;
+        // I don't know why I had to put the jump() call here
         // But if we move it into FixedUpdate it's not responsive 
-        if (_canJump) Jump();
+        if (_can_jump) jump();
     }
 
     private void FixedUpdate()
     {
-        CheckGroundCollision();
-        MoveCharacter();
-        if (_onGround)
+        check_ground_collision();
+        move_character();
+        
+        if (_on_ground)
         {
-            ApplyGroundLinearDrag();
-            _extraJumpsValue = _extraJumps;
+            apply_ground_linear_drag();
             crouch();
+            _extra_jumps_count = _extra_jumps;
+            _rb.gravityScale = 1f;
         }
         else
         {
-            ApplyAirLinearDrag();
-            FallMultiplier();
+            apply_air_linear_drag();
+            set_gravity();
             fast_fall();
         }
 
     }
 
-    private Vector2 GetInput()
+    private Vector2 get_input()
     {
         return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
-    private void MoveCharacter()
+    //Move the character on the horizontal direction
+    private void move_character()
     {
-        _rb.AddForce(new Vector2(_horizontalDirection, 0f) * _movementAcceleration);
+        _rb.AddForce(new Vector2(_horizontal_direction, 0f) * _movement_acceleration);
 
-        if(Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
+        if(Mathf.Abs(_rb.velocity.x) > _max_move_speed)
         {
-            _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _maxMoveSpeed, _rb.velocity.y);
+            _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _max_move_speed, _rb.velocity.y);
         }
     }
 
@@ -99,11 +103,11 @@ public class Movement2D : MonoBehaviour
     }
 
 
-    private void ApplyGroundLinearDrag()
+    private void apply_ground_linear_drag()
     {
-        if(Mathf.Abs(_horizontalDirection) < 0.4f || _changingDirection)
+        if(Mathf.Abs(_horizontal_direction) < 0.4f || _changing_direction)
         {
-            _rb.drag = _groundLinearDrag;
+            _rb.drag = _ground_linear_drag;
         }
         else
         {
@@ -111,14 +115,18 @@ public class Movement2D : MonoBehaviour
         }
     }
 
-     private void ApplyAirLinearDrag()
+     private void apply_air_linear_drag()
     {
-        _rb.drag = _airLinearDrag;
+        _rb.drag = _air_linear_drag;
     }
 
-    private void Jump()
+    private void jump()
     {
-        if (!_onGround)
+        if (!_on_ground)
+        {
+            _extra_jumps_count--;
+        }
+
         //If the player is crouching, the player jump a little bit higher (25% as we reduce his size by 50% (25% top side, 25% bot side))
         if (_is_crouching)
         {
@@ -133,16 +141,18 @@ public class Movement2D : MonoBehaviour
         
     }
 
-    private void FallMultiplier()
-    {
+    private void set_gravity()
+    {    
+        //jump
         if(_rb.velocity.y < 0f)
         {
-            _rb.gravityScale = _fallMultiplier;
+            _rb.gravityScale = _fall_gravity;
         }
+        //jump Cut
         else if ( _rb.velocity.y > 0 && !Input.GetButton("Jump"))
         {
-            _rb.gravityScale = _lowJumpFallMultiplier;
-        }
+            _rb.gravityScale = _low_jump_fall_gravity;
+        }      
         else
         {
             _rb.gravityScale = 1f;
@@ -164,12 +174,10 @@ public class Movement2D : MonoBehaviour
         }
     }
 
-    private void CheckGroundCollision()
+    private void check_ground_collision()
     {
         // To check the ground collision, we cast rays from the player to the ground. One in front of the player
         // And the other in his back to still detect the ground if the player is on the edge of the ground.
-        _onGround = Physics2D.Raycast(transform.position + _groundRaycastOffset , Vector2.down, _groundRaycastLength, _groundLayer) ||
-                    Physics2D.Raycast(transform.position - _groundRaycastOffset , Vector2.down, _groundRaycastLength, _groundLayer);
         if(_is_crouching)
         {
             _on_ground = Physics2D.Raycast(transform.position + _ground_raycast_offset , Vector2.down, _ground_raycast_length * 0.5f, _ground_layer) ||
@@ -180,14 +188,13 @@ public class Movement2D : MonoBehaviour
             _on_ground = Physics2D.Raycast(transform.position + _ground_raycast_offset , Vector2.down, _ground_raycast_length, _ground_layer) ||
                     Physics2D.Raycast(transform.position - _ground_raycast_offset , Vector2.down, _ground_raycast_length, _ground_layer);
         }
+        
     }
 
     //These guizmos show the ray that is cast to check the ground collision
     private void OnDrawGizmos() 
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position + _groundRaycastOffset, transform.position + _groundRaycastOffset + Vector3.down * _groundRaycastLength);
-        Gizmos.DrawLine(transform.position - _groundRaycastOffset, transform.position - _groundRaycastOffset + Vector3.down * _groundRaycastLength);    
         if(_is_crouching)
         {
             Gizmos.DrawLine(transform.position + _ground_raycast_offset, transform.position + _ground_raycast_offset + Vector3.down * _ground_raycast_length * 0.5f);
@@ -198,6 +205,7 @@ public class Movement2D : MonoBehaviour
             Gizmos.DrawLine(transform.position + _ground_raycast_offset, transform.position + _ground_raycast_offset + Vector3.down * _ground_raycast_length);
             Gizmos.DrawLine(transform.position - _ground_raycast_offset, transform.position - _ground_raycast_offset + Vector3.down * _ground_raycast_length); 
         }
+         
 
     }
 }
