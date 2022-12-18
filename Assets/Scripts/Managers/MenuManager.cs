5b13609e44c0dbb4ceea4698bc3961918f9008d1
TLORENT <lorent.tanguy@gmail.com>
2022-12-19 00:58:47 +0100

    ✨ Implemented AudioSystem

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuManager : MonoBehaviour
+public class MenuManager : Singleton<MenuManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Page : MonoBehaviour
+public class Menu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
