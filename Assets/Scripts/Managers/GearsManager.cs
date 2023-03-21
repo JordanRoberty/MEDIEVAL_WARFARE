27959d604fca0d6b7f733b5b76c90285ac3994a0
TLORENT <lorent.tanguy@gmail.com>
2023-03-21 12:34:17 +0100

    ✨ [GearManager] display equiped runes

--- Assets/Scripts/Managers/GearsManager.cs
+++ Assets/Scripts/Managers/GearsManager.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.GameFoundation;
 using UnityEngine.GameFoundation.Components;
+using static GameObjectUtils;
 using static GameFoundationUtils;
 
 public class GearsManager : Singleton<GearsManager>
 {
+    [Header("UI prefabs")]
+    [SerializeField]
+    private GameObject rune_viewer_prefab;
+
+    [Header("Gears menu elements")]
     [SerializeField] private ItemView _equiped_weapon_viewer;
+    [SerializeField] private Transform _equiped_runes_viewer;
 
     void Start()
     {
-        display_equiped_weapon();
-        //display_equiped_runes();
+        display_equiped_gear();
     }
 
-    void display_equiped_weapon()
+    void display_equiped_gear()
     {
+        // DISPLAY WEAPON
         InventoryItem equiped_weapon = PlayerInfosManager.Instance.equiped_weapon;
 
-        Debug.Log("Current weapon : " + equiped_weapon.definition.displayName);
+        display_item_in_viewer(equiped_weapon, _equiped_weapon_viewer);
 
-        _equiped_weapon_viewer.SetDisplayName(equiped_weapon.definition.displayName);
-        display_item_icon_in_viewer(equiped_weapon, _equiped_weapon_viewer);
-        _equiped_weapon_viewer.SetDescription("");
+        // DISPLAY RUNES
+        int nb_rune_slots = equiped_weapon.GetMutableProperty("nb_rune_slots");
+        List <InventoryItem> equiped_runes = PlayerInfosManager.Instance.equiped_runes;
+
+        // Clear viewer
+        _equiped_runes_viewer.destroy_children();
+
+        // Display rune slots and equiped runes
+        for (int rune_slot=0; rune_slot < nb_rune_slots; rune_slot++)
+        {
+            ItemView rune_viewer = Instantiate(
+                rune_viewer_prefab,
+                Vector3.zero,
+                Quaternion.identity,
+                _equiped_runes_viewer
+            ).GetComponent<ItemView>();
+
+            if(rune_slot < equiped_runes.Count)
+            {
+                display_item_in_viewer(equiped_runes[rune_slot], rune_viewer);
+            }
+        }
     }
 }
