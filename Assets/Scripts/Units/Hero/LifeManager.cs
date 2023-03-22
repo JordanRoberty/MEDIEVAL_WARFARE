5e7d7faac5ab61db3cdf26650615f461f53d4448
Emma <emma.pernin1@gmail.com>
2023-03-22 16:43:47 +0100

    ✨ [Game Over] Can go to the main menu

--- Assets/Scripts/Units/Hero/LifeManager.cs
+++ Assets/Scripts/Units/Hero/LifeManager.cs
@@ -11,6 +11,7 @@ public class LifeManager : MonoBehaviour
     {
         player_data = GetComponent<PlayerData>();
         invincible = GetComponent<Invincibility>();
+        Debug.Log("Start !");
     }
 
     void Update()
@@ -19,9 +20,12 @@ public class LifeManager : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Collision !");
+        Debug.Log(collision.gameObject.tag);
         // Si le collider en contact est l'ennemi
         if (collision.gameObject.tag == "Enemy")
         {
+            Debug.Log("Ouch !");
             // Réduction de la vie du personnage et rendu invincible pendant 2 secondes
             player_data.health -= collision.gameObject.GetComponent<Enemy>().get_damage();
             invincible.get_invulnerable();
