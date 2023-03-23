d8a6b5eb7c068147143fe73066f034597b3e44aa
Mr Lorent <lorent.tanguy@gmail.com>
2023-03-23 21:02:27 +0100

    🧼[FirAim] repair firAim

--- Assets/Scripts/Units/Camera/CrushPlayer.cs
+++ Assets/Scripts/Units/Camera/CrushPlayer.cs
@@ -7,31 +7,21 @@ public class CrushPlayer : MonoBehaviour
     [SerializeField] private PlayerData player;
     [SerializeField] private float _raycast_length = 0.3f;
     [SerializeField] private LayerMask _player_layer;
+    
 
-    private bool is_crush = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //Debug.Log(player.transform.position);
-        is_crushed();
-        if(is_crush){
+        
+        if(is_crushed())
+        {
             player.health = 0;
         }
     }
 
 
-    private void is_crushed()
+    private bool is_crushed()
     {
-        is_crush = Physics2D.Raycast(new Vector3(transform.position.x, player.transform.position.y, transform.position.z), -Vector2.left, _raycast_length , _player_layer);
-        //Debug.Log(Physics2D.Raycast(new Vector2(transform.position.x, player.transform.position.y, transform.position.z), -Vector2.left, _raycast_length , _player_layer) );
-        
-        
+        return Physics2D.Raycast(new Vector3(transform.position.x, player.transform.position.y, transform.position.z), -Vector2.left, _raycast_length , _player_layer);
     }
 
     private void OnDrawGizmos() 
@@ -42,7 +32,5 @@ public class CrushPlayer : MonoBehaviour
             Gizmos.DrawLine(position, position  - Vector3.left * _raycast_length);
  
         }
-         
-
     }
 }
--- Assets/Scripts/Weapon/FireAim.cs
+++ Assets/Scripts/Weapon/FireAim.cs
@@ -16,20 +16,7 @@ public class FireAim : MonoBehaviour
 
     void Update()
     {
-        /*if (Input.GetKeyDown("up"))
-        {
-            gameObject.transform.Translate(0.0f,0.5f,0,Space.Self);
-            gameObject.transform.Rotate(0.0f, 0.0f, 45.0f, Space.Self);
-        }
-
-        if (Input.GetKeyUp("up"))
-        {
-            gameObject.transform.Translate(0.0f, -0.5f, 0, Space.Self);
-            gameObject.transform.Rotate(0.0f, 0.0f, -45.0f, Space.Self);
-        }*/
-
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        // Debug.Log(mousePos);
 
         Vector3 aimDirection = mousePos - Player.transform.position;
         float rotZ = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
