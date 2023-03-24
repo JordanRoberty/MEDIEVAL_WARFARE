a0279e14fbc80685e5d1c4a4fa016739ab6b8d85
badbois <jordan.roberty@outlook.fr>
2023-03-24 23:42:32 +0100

    ✨ [UIMANAGER] UI now display coins that we loot (but still need to read from GF wallet)

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -7,10 +7,14 @@ public class UIManager : Singleton<UIManager>
 {
     public int score = 0;
     [SerializeField] private TextMeshProUGUI score_text;
+
+    public int nb_coins = 0;//replace by the nmber of coins in the wallet
+    [SerializeField] private TextMeshProUGUI coins_text;
     // Start is called before the first frame update
     void Start()
     {
         score_text.SetText("SCORE : " + score);
+        coins_text.SetText("MONEY : " + nb_coins);
     }
 
     // Update is called once per frame
@@ -23,5 +27,11 @@ public class UIManager : Singleton<UIManager>
     {
         score += point;
         score_text.SetText("SCORE : " + score);
+    }
+
+    public void update_coins(int coins)
+    {
+        nb_coins += coins;
+        coins_text.SetText("MONEY : " + nb_coins);
     }
 }
