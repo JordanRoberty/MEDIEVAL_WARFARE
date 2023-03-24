664da5d6f6c26ca31e2b5d5d2dbe496cc29a9032
Mr Lorent <lorent.tanguy@gmail.com>
2023-03-24 22:38:51 +0100

    ♻️ [WeaponManager] Simplify weapon pipe for next weapons

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -1,65 +1,8 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.GameFoundation;
-using UnityEngine.Assertions;
-using UnityEngine.GameFoundation.Components;
-using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject Player;
-    private Vector3 mousePos;
-    private Camera mainCam;
-    private float shot_freq;
-    private GameObject bullet_prefab;
-    private SpriteRenderer renderer;
-
-    void Start()
-    {
-        renderer = gameObject.GetComponent<SpriteRenderer>();
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        InventoryItem weapon = PlayerInfosManager.Instance.equiped_weapon;
-        AsyncOperationHandle<Sprite> load_sprite = weapon.definition.GetStaticProperty("sprite").AsAddressable<Sprite>();
-        load_sprite.Completed += (AsyncOperationHandle<Sprite> handle) =>
-        {
-            if (handle.Status == AsyncOperationStatus.Succeeded)
-            {
-                Sprite sprite = handle.Result;
-                Assert.IsNotNull(sprite);
-                renderer.sprite = sprite;
-            }
-        };
-
-        shot_freq = weapon.definition.GetStaticProperty("shot_freq");
-
-        AsyncOperationHandle<GameObject> load_bullet = weapon.definition.GetStaticProperty("bullet_prefab").AsAddressable<GameObject>();
-        load_bullet.Completed += (AsyncOperationHandle<GameObject> handle) =>
-        {
-            if (handle.Status == AsyncOperationStatus.Succeeded)
-            {
-                GameObject bullet = handle.Result;
-                Assert.IsNotNull(bullet);
-                bullet_prefab = bullet;
-                InvokeRepeating("Shoot", 0f, shot_freq);
-            }
-        };
-
-        
-    }
-
-    void Update()
-    {
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-
-        Vector3 aimDirection = mousePos - Player.transform.position;
-        float rotZ = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-
-        transform.rotation = Quaternion.Euler(0, 0, rotZ);
-    }
-
-    void Shoot()
-    {
-        Instantiate(bullet_prefab, gameObject.transform.position, gameObject.transform.rotation);
-    }
+    public Transform cannon_end;
 }
---
+++ Assets/Scripts/Weapon/WeaponManager.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.GameFoundation;
+using UnityEngine.Assertions;
+using UnityEngine.GameFoundation.Components;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class WeaponManager : MonoBehaviour
+{
+    private Camera _main_cam;
+    private Vector3 mouse_position;
+
+    private InventoryItem _weapon_infos;
+    private GameObject _weapon_prefab;
+
+    private Weapon _current_weapon;
+    private float shot_freq;
+    private GameObject bullet_prefab;
+    [SerializeField] private Transform _bullet_container;
+
+    void Awake()
+    {
+        _main_cam = Camera.main;
+        _weapon_infos = PlayerInfosManager.Instance.equiped_weapon;
+        shot_freq = _weapon_infos.definition.GetStaticProperty("firing_rate");
+
+        load_weapon();
+    }
+
+    private void load_weapon()
+    {
+        AsyncOperationHandle<GameObject> load_weapon_prefab = _weapon_infos.definition.GetStaticProperty("weapon_prefab").AsAddressable<GameObject>();
+        load_weapon_prefab.Completed += (AsyncOperationHandle<GameObject> handle) =>
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                Assert.IsNotNull(handle.Result);
+                _weapon_prefab = handle.Result;
+
+                _current_weapon = Instantiate(
+                    _weapon_prefab,
+                    transform.position,
+                    Quaternion.identity,
+                    transform
+                ).GetComponent<Weapon>();
+
+                Assert.IsNotNull(_current_weapon);
+
+                load_bullet();
+            }
+        };
+    }
+
+    private void load_bullet()
+    {
+        AsyncOperationHandle<GameObject> load_bullet_prefab = _weapon_infos.definition.GetStaticProperty("bullet_prefab").AsAddressable<GameObject>();
+        load_bullet_prefab.Completed += (AsyncOperationHandle<GameObject> handle) =>
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                Assert.IsNotNull(handle.Result);
+                bullet_prefab = handle.Result;
+                init();
+            }
+        };
+    }
+
+    private void init()
+    {
+        InvokeRepeating("Shoot", 0f, shot_freq);
+    }
+
+    void Update()
+    {
+        mouse_position = _main_cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 aim_direction = mouse_position - transform.position;
+        float shoulder_rotation = Mathf.Atan2(aim_direction.y, aim_direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, shoulder_rotation);
+    }
+
+    void Shoot()
+    {
+        Instantiate(
+            bullet_prefab,
+            _current_weapon.cannon_end.position,
+            _current_weapon.cannon_end.rotation,
+            _bullet_container
+        );
+    }
+}
