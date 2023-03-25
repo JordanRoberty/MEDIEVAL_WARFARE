40c01513f3093bf91ed5bdeab489f8beca7abad8
badbois <jordan.roberty@outlook.fr>
2023-03-25 15:37:19 +0100

    ✨ [UI] Display armor

--- Assets/Scripts/UI/Health/displayLife.cs
+++ Assets/Scripts/UI/Health/displayLife.cs
@@ -3,45 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class displayLife : MonoBehaviour
+
+public class DisplayLife : MonoBehaviour
 {
     private int health;
     private int numOfHeart;
+    private int shield;
+    private int maximum_health;
 
     public GameObject player;
 
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite emptyHeart;
+    public Sprite shieldHeart;
 
-    private void Start()
+    public void Awake()
     {
         health = player.GetComponent<PlayerManager>().health;
         numOfHeart = player.GetComponent<PlayerManager>().max_health;
+        shield = player.GetComponent<PlayerManager>().shield;
+
     }
 
     void Update()
     {
 
         health = player.GetComponent<PlayerManager>().health;
+        shield = player.GetComponent<PlayerManager>().shield;
+        numOfHeart = player.GetComponent<PlayerManager>().max_health + shield;
 
         if (health > numOfHeart)
         {
             health = numOfHeart;
         }
 
-        for (int i = 0; i < hearts.Length; i++)
+        int current_life = health + shield;
+
+        for (int i = 0; i < hearts.Length ; i++)
         {
-            if (i < health)
+
+            //set the sprite of the heart
+            if (i < health )
             {
                 hearts[i].sprite = fullHeart;
+
             }
             else
             {
-                hearts[i].sprite = emptyHeart;
+                if(shield > 0){
+                    hearts[i].sprite = shieldHeart;
+                    shield--;
+                }else
+                {
+                    hearts[i].sprite = emptyHeart;
+                }       
             }
 
-            if (i < numOfHeart)
+            //set the heart active or not
+            if (i < current_life)
             {
                 hearts[i].enabled = true;
             }
@@ -51,5 +71,4 @@ public class displayLife : MonoBehaviour
             }
         }
     }
-
 }
