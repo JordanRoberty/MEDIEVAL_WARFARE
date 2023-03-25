0c32ce2180dfc1fe1589b87d31b2aebf566860b3
Fabian SANTIAGO <fabiansantiago467@gmail.com>
2023-03-25 23:45:49 +0100

    🚧 [BOSS] Added new animations and new links for the boss, added hitboxes for the weapon

--- Assets/Scripts/Units/Enemies/Boss.cs
+++ Assets/Scripts/Units/Enemies/Boss.cs
@@ -13,7 +13,7 @@ public class Boss : Enemy
 
     private float bottle_range = 12.0f;
     private float flail_range = 9.0f;
-    private GameObject player;
+    public GameObject player;
     public GameObject bottle_prefab;
     public float timer_before_action = 1.0f;
     public float time_in_air = 1.0f;
@@ -21,6 +21,8 @@ public class Boss : Enemy
     public LayerMask ground;
     private bool is_facing = true;
     private Vector3 look_at = Vector3.left;
+    public Animator animator;
+    private float moveSpeedThreshold = 0.1f;
 
     private void Start()
     {
@@ -71,112 +73,72 @@ public class Boss : Enemy
                 timer_before_action = Random.Range(2f, 4f);
                 if (Random.value < 0.5f)
                 {
-                    // TODO (Fabian) : Coup de massue ici
-                    Invoke("start_flail_attack", 0.0f);
+                    animator.SetBool("Horizontal_attack", true);
+                    StartCoroutine(DisableHorizontalAttackAfterAnimation());
                 }
                 else
                 {
-                    // TODO (Fabian) : Masse tendue vers l'avant
-                    Debug.Log("Lunging Strike");
+                    animator.SetBool("Vertical_attack", true);
+                    StartCoroutine(DisableVerticalAttack());
                 }
             }
-            else if (distance_to_player <= bottle_range && timer_before_action < 0.0f)
+
+            if (distance_to_player <= bottle_range && timer_before_action< 0.0f)
             {
+                animator.SetBool("Going_forward", false);
                 timer_before_action = Random.Range(2f, 4f);
-                if (Random.value < 0.5f)
+                if (Random.value< 0.5f)
                 {
                     throw_bottle();
-                }
+            }
                 else
-                {
-                    jump_on_player();
-                }
+            {
+                jump_on_player();
+                animator.SetBool("IsAiborn", false);
+            }
             }
             else if (distance_to_player > bottle_range)
             {
-                // on n'est pas assez proche pour attaquer, on se déplace vers le joueur
-                Vector3 direction_to_player = (
-                    player.transform.position - transform.position
-                ).normalized;
+                // On n'est pas assez proche pour attaquer, on se déplace vers le joueur
+                Vector3 direction_to_player = (player.transform.position - transform.position).normalized;
                 direction_to_player = new Vector3(direction_to_player.x, 0.0f, 0.0f);
-                transform.position += direction_to_player * speed * Time.deltaTime;
-                if (Physics2D.Raycast(transform.position, Vector3.down, 2.5f, ground))
+
+                if (direction_to_player.magnitude > moveSpeedThreshold)
                 {
-                    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    // Le personnage est en train de se déplacer, on met à jour le booléen Going_forward
+                    animator.SetBool("Going_forward", true);
+
+                    transform.position += direction_to_player * speed * Time.deltaTime;
+                    if (distance_to_player <= bottle_range)
+                    {
+                        animator.SetBool("Going_forward", false);
+                    }
+                    if (Physics2D.Raycast(transform.position, Vector3.down, 2.5f, ground))
+                    {
+                        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                        animator.SetBool("Going_forward", false);
+                    }
+                }
+                else
+                {
+                    // Le personnage est immobile, on met à jour le booléen Going_forward
+                    animator.SetBool("Going_forward", false);
                 }
             }
+
         }
     }
 
-    void start_flail_attack()
-    {
-        GameObject arm = GameObject.Find("FirePoint");
-
-        Quaternion startRotation = arm.transform.rotation;
-        Quaternion endRotation = Quaternion.Euler(0f, 0f, 360f);
-        float duration = 2f;
-
-        StartCoroutine(RotateObject(arm, startRotation, endRotation, duration));
-    }
-
-    IEnumerator RotateObject(
-        GameObject obj,
-        Quaternion startRotation,
-        Quaternion endRotation,
-        float duration
-    )
-    {
-        // Vérifier que l'objet est toujours valide
-        if (obj == null)
-        {
-            yield break;
-        }
-
-        float angle = 0f;
-        float totalAngle = 360f; // L'angle total de rotation que l'on souhaite effectuer
-
-        while (angle < totalAngle)
-        {
-            float deltaAngle = Time.deltaTime * 360f / duration; // La quantité de rotation à appliquer à chaque frame
-            angle += deltaAngle;
-
-            // Faire la rotation relative à la rotation actuelle de l'objet
-            obj.transform.Rotate(0f, 0f, deltaAngle, Space.Self);
-            yield return null;
-        }
-
-        // Faire une dernière rotation pour être sûr d'être à la fin de la rotation
-        obj.transform.rotation = endRotation;
-    }
 
     void throw_bottle()
     {
-        // On récupère la position actuelle du joueur et du boss
-        Vector2 player_position = player.transform.position;
-        Vector2 boss_position = transform.position;
-
-        // On calcule la direction et la distance entre le boss et le joueur
-        Vector2 direction_to_player = player_position - boss_position;
-        float distance_to_player = direction_to_player.magnitude;
-
-        // On calcule la position de départ de la bouteille
-        Vector2 bottle_start_position = boss_position + direction_to_player.normalized * 2.0f;
-
-        // On instancie la bouteille à la position de départ
-        GameObject bottle = Instantiate(bottle_prefab, bottle_start_position, Quaternion.identity);
-
-        // On calcule la direction de lancement de la bouteille en ajoutant une petite variation
-        Vector2 bottle_direction =
-            direction_to_player.normalized
-            + new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
-
-        // On applique une force au rigidbody de la bouteille pour la lancer
-        Rigidbody2D bottle_rigidbody = bottle.GetComponent<Rigidbody2D>();
-        bottle_rigidbody.AddForce(bottle_direction * 10f, ForceMode2D.Impulse);
+        animator.SetBool("Bottle_toss", true);
+        StartCoroutine(ThrowBottle());
     }
 
     void jump_on_player()
     {
+        animator.SetBool("IsAirborn", true);
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         rigidbody.velocity = Vector2.zero;
         // la position actuelle du personnage
@@ -236,6 +198,7 @@ public class Boss : Enemy
             rb.velocity = Vector2.zero;
             rb.AddForce(Vector2.down * 50000f, ForceMode2D.Impulse); // Ajouter une force vers le bas
         }
+        animator.SetBool("IsAirborn", false);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -261,11 +224,67 @@ public class Boss : Enemy
             {
                 collision.transform.position += direction * 3f;
             }
-            playerRb.AddForce(direction * 2f , ForceMode2D.Impulse);
+            playerRb.AddForce(direction * 2f, ForceMode2D.Impulse);
         }
         else if (collision.gameObject.name == "Ground")
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
+    }
+
+    IEnumerator DisableHorizontalAttackAfterAnimation()
+    {
+        // Attendre la fin de l'animation
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Désactiver le paramètre "Horizontal_attack"
+        animator.SetBool("Horizontal_attack", false);
+    }
+
+    IEnumerator ThrowBottle()
+    {
+        // Attendre la fin de l'animation
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // On récupère la position actuelle du joueur et du boss
+        Vector2 player_position = player.transform.position;
+        Vector2 boss_position = transform.position;
+
+        // On calcule la direction et la distance entre le boss et le joueur
+        Vector2 direction_to_player = player_position - boss_position;
+        float distance_to_player = direction_to_player.magnitude;
+
+        // On calcule la position de départ de la bouteille
+        Vector2 bottle_start_position = boss_position + direction_to_player.normalized * 2.0f;
+
+        // On instancie la bouteille à la position de départ
+        GameObject bottle = Instantiate(bottle_prefab, bottle_start_position, Quaternion.identity);
+
+        // On calcule la direction de lancement de la bouteille en ajoutant une petite variation
+        Vector2 bottle_direction =
+            direction_to_player.normalized
+            + new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+
+        // On applique une force au rigidbody de la bouteille pour la lancer
+        Rigidbody2D bottle_rigidbody = bottle.GetComponent<Rigidbody2D>();
+        bottle_rigidbody.AddForce(bottle_direction * 10f, ForceMode2D.Impulse);
+        animator.SetBool("Bottle_toss", false);
+    }
+
+    IEnumerator DisableVerticalAttack()
+    {
+        // Attendre la fin de l'animation
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Désactiver le paramètre "Horizontal_attack"
+        animator.SetBool("Vertical_attack", false);
+    }
+    IEnumerator DisableWalk()
+    {
+        // Attendre la fin de l'animation
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Désactiver le paramètre "Horizontal_attack"
+        animator.SetBool("Going_forward", false);
     }
 }
