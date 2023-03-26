b1a136dd0eecf4e26aa4acb3772496c7636c491e
Mr Lorent <lorent.tanguy@gmail.com>
2023-03-26 01:42:35 +0100

    ♻️ [Player] Put player into it's own scene

--- Assets/Scripts/Units/Camera/CrushPlayer.cs
+++ Assets/Scripts/Units/Camera/CrushPlayer.cs
@@ -4,31 +4,33 @@ using UnityEngine;
 
 public class CrushPlayer : MonoBehaviour
 {
-    [SerializeField] private PlayerManager _player;
     [SerializeField] private float _raycast_length = 0.3f;
     [SerializeField] private LayerMask _player_layer;
     
 
     void FixedUpdate()
     {
-        if(is_crushed() && GameManager.Instance._state == GameState.RUNNING)
+        if(PlayerManager.Instance != null && is_crushed() && GameManager.Instance._state == GameState.RUNNING)
         {
-            _player.die();
+            PlayerManager.Instance.die();
         }
     }
 
 
     private bool is_crushed()
     {
-        return Physics2D.Raycast(new Vector3(transform.position.x, _player.transform.position.y, transform.position.z), -Vector2.left, _raycast_length , _player_layer);
+        return Physics2D.Raycast(new Vector3(transform.position.x, PlayerManager.Instance.transform.position.y, transform.position.z), -Vector2.left, _raycast_length , _player_layer);
     }
 
     private void OnDrawGizmos() 
     {
-        Gizmos.color = Color.red;
-        Vector3 position = new Vector3(transform.position.x, _player.transform.position.y, transform.position.z);
+        if(PlayerManager.Instance != null)
         {
-            Gizmos.DrawLine(position, position  - Vector3.left * _raycast_length);
+            Gizmos.color = Color.red;
+            Vector3 position = new Vector3(transform.position.x, PlayerManager.Instance.transform.position.y, transform.position.z);
+            {
+                Gizmos.DrawLine(position, position - Vector3.left * _raycast_length);
+            }
         }
     }
 }
