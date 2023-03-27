3859e7d805692a4e39f594424137a4420d7dd617
Mr Lorent <lorent.tanguy@gmail.com>
2023-03-27 17:16:31 +0200

    ♻️ [Boss_Level_1] Boss is now init by BossLevelLoader

--- Assets/Scripts/Core/BossLevelLoader.cs
+++ Assets/Scripts/Core/BossLevelLoader.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BossLevelLoader : LevelLoader
 {
+    [SerializeField] private Boss _boss;
+
     public override void init()
     {
         base.init();
         _player_manager.update_dependencies(_main_camera);
         _player_manager.gameObject.SetActive(true);
+
+        _boss.init(_player_manager.transform);
     }
 }
